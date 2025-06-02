6f8aa462eff81bacf9673882a984207f725b4d89
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2025-06-01 22:13:57 -0600

    Popular View added.

---
+++ lib/presentation/views/movies/popular_view.dart
@@ -0,0 +1,31 @@
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PopularView extends ConsumerStatefulWidget {
+  const PopularView({super.key});
+
+  @override
+  PopularViewState createState() => PopularViewState();
+}
+
+class PopularViewState extends ConsumerState<PopularView> {
+  @override
+  Widget build(BuildContext context) {
+    final popularMovies = ref.watch(popularMoviesProvider);
+
+    if(popularMovies.isEmpty) {
+      return const Center(
+        child: CircularProgressIndicator(strokeWidth: 2),
+      );
+    }
+
+    return Scaffold(
+      body: MovieMasonry(
+        movies: popularMovies,
+        loadNextPage: ref.read(popularMoviesProvider.notifier).loadNextPage,
+      ),
+    );
+  }
+}
